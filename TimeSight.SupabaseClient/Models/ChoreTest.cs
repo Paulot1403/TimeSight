3c5385e99b3f640997b910f0e66e2cb8f7305944
Paul Chaumont <pchaumont@ensc.fr>
2026-05-31 08:12:15 +0200

    Auto commit 2026-05-31 08:12:14

---
+++ TimeSight.SupabaseClient/Models/ChoreTest.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeSight.Models;
+
+public class ChoreTest
+{
+    public string Test { get; set; }
+}
