10fd77b5fc3e12c65445451a3fd67a8fb7b49757
Paul Chaumont <pchaumont@ensc.fr>
2026-06-28 15:27:23 +0200

    fix

--- TimeSight/Program.cs
+++ TimeSight/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 using TimeSight;
 using TimeSight.repositories;
 using TimeSight.Services;
